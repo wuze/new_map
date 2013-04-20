3b0d8f00c9c1225eba8fa3c8c29f7bc4657820f8
Little Black <wuze000@gmail.com>
2013-04-20 11:13:32 +0800

    add files

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.map.tmc/houtai2';
+$config['base_url']	= 'http://www.map.tmc/admin';
 
 /*
 |--------------------------------------------------------------------------
